922086c32c14d644bba846d365620298005d9a21
Limon <limon4u714@gmail.com>
2023-07-02 00:27:28 +0600

    layout update

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -7,11 +7,7 @@ export default function LayoutForAbout({children}) {
     const pathroute = usePathname();
   return (
     <div>
-        {/* {
-            pathroute !== "/about/user" ?
-            <h2>Common Layout for About and User Page</h2>
-            : <Link href={"/"}> Go to Home Page </Link>
-        } */}
+      
       {children}
     </div>
   )
