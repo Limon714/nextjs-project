687834745876dc54b28c2602f72f535e439f2761
Limon <limon4u714@gmail.com>
2023-07-02 14:46:11 +0600

    font optimize

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,29 @@
 "use client"
-import Image from "next/image"
-import Profile from '../../public/vercel.svg'
-import Link from "next/link"
 import txt from './text.module.css'
 import { useRouter } from "next/navigation"
 
+import { Fasthand } from 'next/font/google';
+ const myFont = Fasthand({
+      weight:'400',
+      display:'swap',
+      subsets:['latin'],
+ })
 
 export default function Page() {
-  console.log(Profile)
+
   const router = useRouter();
   const navigate = (path)=>{
     router.push(path)
   }
   return (
-    <div>
-      <h1 className={txt.hello}> Next js | Deploy to Vercel </h1>
-      <Image src={Profile}  alt="Image of the author" className="img"/>
-     
-{/* 
+    <>
+    <div className={txt.hello}>
+      <h1> Next js | Font Optimization </h1>
      <br />
      <br />
-     <button onClick={()=>navigate("/about")}>Go to about page</button>
-     <button onClick={()=>navigate("/service")}>Go to Service page</button> */}
+     <h1 className={myFont.className}>Learn, Practise, work with Nextjs 13.4</h1>
+
     </div>
+    </>
   )
 }
