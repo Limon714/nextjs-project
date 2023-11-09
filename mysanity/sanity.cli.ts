3d732cd30ece478c0d9244a3c0165e5e99765152
Limon Islam <limon4u714@gmail.com>
2023-11-09 22:22:02 +0600

    Next js 14 Ecommerce Project

---
+++ mysanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'moek1g1p',
+    dataset: 'production'
+  }
+})
--- src/app/service/quality/[...value]/page.js
+++
@@ -1,9 +0,0 @@
-export default function Value({params}) {
-    return (
-      <div>
-        <h2>Product value is {params.value [0]}</h2>
-        <h2>Product value is {params.value [1]}</h2>
-        <h2>Product value is {params.value [2]}</h2>
-      </div>
-    )
-  }
